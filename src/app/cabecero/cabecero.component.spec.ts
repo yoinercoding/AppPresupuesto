0514cc2ec177207d129c1fdb46572644c69649a0
Yoiner Bello Fajardo <ybello@imasa.com.ar>
2022-06-14 14:48:50 -0300

    Primera subida

---
+++ src/app/cabecero/cabecero.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CabeceroComponent } from './cabecero.component';
+
+describe('CabeceroComponent', () => {
+  let component: CabeceroComponent;
+  let fixture: ComponentFixture<CabeceroComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CabeceroComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CabeceroComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/egreso/egreso.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EgresoComponent } from './egreso.component';
+
+describe('EgresoComponent', () => {
+  let component: EgresoComponent;
+  let fixture: ComponentFixture<EgresoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EgresoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EgresoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
