9d7029ad95396451f617bd2f8be32e22c575f60d
Yoiner Bello Fajardo <ybello@imasa.com.ar>
2022-06-14 15:07:43 -0300

    Se agrega los service al Providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { CabeceroComponent } from './cabecero/cabecero.component';
 import { IngresoComponent } from './ingreso/ingreso.component';
 import { EgresoComponent } from './egreso/egreso.component';
 import { FormularioComponent } from './formulario/formulario.component';
+import { IngresoService } from './ingreso/ingreso.service';
+import { EgresoService } from './egreso/egreso.service';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,7 @@ import { FormularioComponent } from './formulario/formulario.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [IngresoService, EgresoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
